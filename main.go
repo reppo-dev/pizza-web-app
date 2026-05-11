946f888ca96ea2b181fe2a47d5b97e997cdc8974
soyr_6o <soyrozero@gmail.com>
2026-05-11 15:43:51 +0330

    adding router

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"backend/databases"
+	"backend/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	databases.Conect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000/",
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
 
 	app.Listen(":8000")
 }
