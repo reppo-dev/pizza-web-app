2c0da248176b1de5907e906ee683c24e3353f1c2
soyr_6o <soyrozero@gmail.com>
2026-05-16 10:50:27 +0330

    backend:adding pizza update controller

--- backend/controller/pizzaController.go
+++ backend/controller/pizzaController.go
@@ -4,6 +4,7 @@ import (
 	"backend/databases"
 	"backend/models"
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -42,3 +43,28 @@ func CreatePizza(c *fiber.Ctx) error {
 	return c.JSON(pizza)
 }
 
+func UpdatePizza(c *fiber.Ctx) error {
+
+	id , _ := strconv.Atoi(c.Params("id"))
+	var data models.PizzasCreatore
+
+	ctx , cancel := context.WithTimeout(context.Background(),10 * time.Second)
+	defer cancel()
+	c.BodyParser(&data)
+
+	var pizza models.Pizzas
+
+	
+	databases.DB.WithContext(ctx).First(&pizza,id)
+
+    pizza.Name = data.Name
+    pizza.Description = data.Description
+    pizza.Image = data.Image
+    pizza.Status = data.Status
+
+	databases.DB.WithContext(ctx).Save(&pizza)
+
+
+
+	return c.JSON(pizza)
+}
