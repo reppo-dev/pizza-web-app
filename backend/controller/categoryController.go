ae09b98ba4a23b3a5805c3649d0fe19d469eafaf
soyr_6o <soyrozero@gmail.com>
2026-05-18 08:37:10 +0330

    adding controller get category by id

--- backend/controller/categoryController.go
+++ backend/controller/categoryController.go
@@ -4,6 +4,7 @@ import (
 	"backend/databases"
 	"backend/models"
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -17,6 +18,30 @@ func AllCategorys(c *fiber.Ctx) error {
 	if err := databases.DB.WithContext(ctx).Find(&category).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error":"Failed to get category",
+		})
+	}
+
+	return c.JSON(category)
+}
+
+func GetCategory(c *fiber.Ctx) error {
+	ctx , cancel := context.WithTimeout(context.Background(),10*time.Second)
+	defer cancel()
+
+
+	id,err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":"Invalid request category",
+		})
+	}
+
+	var category models.Categorys
+
+
+	if err := databases.DB.WithContext(ctx).First(&category,id).Error; err != nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error":"Filed to found category",
 		})
 	}
 
