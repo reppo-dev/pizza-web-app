07be6c4b4ec02d4d1db6ce78256eb3541934bba7
soyr_6o <soyrozero@gmail.com>
2026-06-01 08:15:25 +0330

    feat(backend):add get user by id controller

--- backend/controller/userController.go
+++ backend/controller/userController.go
@@ -4,6 +4,7 @@ import (
 	"backend/databases"
 	"backend/models"
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -20,4 +21,22 @@ func AllUsers(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(users)
+}
+
+func User(c *fiber.Ctx) error {
+	ctx , candel := context.WithTimeout(context.Background(),5 * time.Second)
+	defer candel()
+
+	id,err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid request body"})
+	}
+
+	var user models.User
+	
+	if err := databases.DB.WithContext(ctx).First(&user,id).Error;err != nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Failed found user"})
+	}
+
+	return c.JSON(user)
 }
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -35,4 +35,5 @@ func Setup(app *fiber.App) {
 	app.Post("/upload",controller.Upload)
 
 	app.Get("/users",controller.AllUsers)
+	app.Get("/user",controller.User)
 }
