c8a1a6f5a45467837aa271b8bcdd1e03136eb3d1
soyr_6o <soyrozero@gmail.com>
2026-06-02 12:45:53 +0330

    feat(backend):add controller get address by id

--- backend/controller/addresController.go
+++ backend/controller/addresController.go
@@ -5,6 +5,7 @@ import (
 	"backend/models"
 	"backend/utils"
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -25,6 +26,29 @@ func GetAllAddress(c *fiber.Ctx) error {
 	}
 
 	if err := databases.DB.WithContext(ctx).Where("user_id = ?",id).Find(&address).Error;err != nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Failed found address"})
+	}
+
+	return c.JSON(address)
+}
+
+func GetAddress(c *fiber.Ctx) error {
+	ctx,cancel:= context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+
+	cookie := c.Cookies("jwt")
+
+	userId , err := utils.ParseJwt(cookie)
+
+	id , err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid request id"})
+	}
+
+	var address models.Address
+
+	if err := databases.DB.WithContext(ctx).Where("id = ? AND user_id = ?",id,userId).First(&address).Error; err != nil{
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Failed found address"})
 	}
 
--- backend/models/address.go
+++ backend/models/address.go
@@ -14,3 +14,15 @@ type Address struct {
 	PostalCode    string `json:"postal_code"`
 	Notes         string `json:"delivery_notes"`
 }
+
+type InfoAddress struct{
+	UserID 	      uint   `json:"user_id"`
+	FullName      string `json:"full_name"`
+	Phone         string `json:"phone"`
+	Province      string `json:"province"`
+	City          string `json:"city"`
+	Street        string `json:"street"`
+	Apartment 	  string `json:"apartment"`
+	PostalCode    string `json:"postal_code"`
+	Notes         string `json:"delivery_notes"`
+}
