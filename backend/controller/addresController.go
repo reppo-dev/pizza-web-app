6419fc3924a9d6b0d09085fc060bb9b9617cc8b4
soyr_6o <soyrozero@gmail.com>
2026-06-02 12:46:35 +0330

    feat(backend):add controller update address user by id

--- backend/controller/addresController.go
+++ backend/controller/addresController.go
@@ -54,3 +54,46 @@ func GetAddress(c *fiber.Ctx) error {
 
 	return c.JSON(address)
 }
+
+func UpdateAddress(c *fiber.Ctx) error {
+	ctx,cancel:= context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+
+	var data models.InfoAddress
+
+	if err := c.BodyParser(&data); err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid request body"})
+	}
+
+	cookie := c.Cookies("jwt")
+
+	userId , err := utils.ParseJwt(cookie)
+
+	id , err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid request id"})
+	}
+
+	var address models.Address
+
+	if err := databases.DB.WithContext(ctx).Where("id = ? AND user_id = ?",id,userId).First(&address).Error; err != nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Failed found address"})
+	}
+
+	address.FullName = data.FullName
+	address.Phone = data.Phone
+	address.Province = data.Province
+	address.City = data.City
+	address.Street = data.Street
+	address.Apartment = data.Apartment
+	address.PostalCode = data.PostalCode
+	address.Notes = data.Notes
+
+	if err := databases.DB.WithContext(ctx).Save(&address).Error;err != nil{
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error":"Failed update address"})
+	}
+
+	return c.JSON(fiber.Map{"message":"success update address"})
+}
+
