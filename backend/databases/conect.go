048d6a25c219dad2b404d069b9de0a9714eeb530
mohammad amin <soyrozero@gmail.com>
2026-06-14 06:23:43 +0330

    Update

--- backend/databases/conect.go
+++ backend/databases/conect.go
@@ -11,7 +11,9 @@ var DB *gorm.DB
 
 func Conect() {
 
-	dns :="host=localhost user=postgres password=13802002 dbname=pizza port=5432 sslmode=disable"
+	// for example ⭣⭣⭣
+	dns := "host=localhost user=postgres password=******** dbname=shoppy port=5432 sslmode=disable"
+	// dns := "host=your_host user=your_databace password=your_password_databace dbname=name_db port=port_database sslmode=disable"
 
 	db,err := gorm.Open(postgres.Open(dns),&gorm.Config{})
 	if err != nil {
